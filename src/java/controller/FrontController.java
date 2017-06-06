2be27fe486b4ce11f8afd49a1ed0060cbd5c0f11
developpeurcurieux <developpeurcurieux@gmail.com>
2017-06-06 11:59:14 +0200

    modif des noms packages pour faciliter l'intergration

--- src/java/controller/FrontController.java
+++ src/java/controller/FrontController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import beans.Client;
-import modelTablesDB.ClientDAO;
+import model.ClientDAO;
 /**
  *
  * @author cdi213
